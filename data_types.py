4b0b72d7cdb19ea62ebf87e84ce84ae11c77e242
sarkodieakua6-coder <sarkodieakua6@gmail.com>
2026-05-26 19:01:17 +0000

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,60 @@
+#SECTION 1: DATA TYPES EXCERCISE
+#1 Personal Bio Generator
+print("PERSONAL BIO GENERATOR :")
+name="Nana Akua Akyaa Sarkodie"
+age=25
+height=5.9
+favorite_tech_field="Computer hardware and networking"
+student_status="Active student"
+print("My name is",name,"I am",age,"years old, I am",height,"feet tall" ,"my favorite tech field is ",favorite_tech_field,"and Iam an ", student_status)
+
+
+#2. TYpe Checker
+print("TYPE CHECKER :")
+number = 15
+decimal = 2.15
+text = "Solavisetech"
+is_student = True
+print(type(number))
+print(type(decimal))
+print(type(text))
+print(type(is_student))
+
+
+#3 Data Converstion
+print("DATA CONVERSION :")
+integer_num= 10
+float_num = 1.9
+string_num = "20"
+
+# Convert integer to string
+converted_string = str(integer_num)
+
+# Convert float to integer
+converted_integer = int(float_num)
+
+# Convert string number to integer
+converted_string_to_integer = int(string_num)
+
+print("Converted integer to string:", converted_string)
+print("Converted float to integer:", converted_integer)
+print("Converted string number to integer:", converted_string_to_integer)
+ 
+   
+#4. User Information
+print("PERSONAL INFORMATION :")
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+country = input("Enter your country: ")
+print("Your name is", name)
+print("You are", age, "years old")
+print("And you are from ", country)
+
+
+#5.Temperature Converter
+print("TEMPERATURE CONVERTER :")
+celsius = float(input("Enter your temperature in Celsius: "))
+fahrenheit = (celsius * 9/5) + 32
+print(" Your temperature in Fahrenheit:", fahrenheit)
+  
+   
